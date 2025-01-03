0c98e496bc6fb4ec9729a5a0a30534cc659349d7
carlos <carlosviniciogarcia1997@hotmail.com>
2025-01-03 14:51:05 -0500

    agora Icon showed in editor

--- plugin/src/blocks/block-one/index.js
+++ plugin/src/blocks/block-one/index.js
@@ -1,9 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
 import edit from './edit';
 
+import AgoraIcon from '../../assets/agoraIcon';
+
 registerBlockType('my-plugin/block-one', {
     title: 'Block One',
     category: 'widgets',
     edit,
+    icon: AgoraIcon,
     save: () => null, // Renderizado dinámico, no guardar salida en el cliente
 });
--- plugin/src/blocks/block-two/index.js
+++ plugin/src/blocks/block-two/index.js
@@ -2,11 +2,13 @@ import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 import Edit from './edit';
 
+import AgoraIcon from '../../assets/agoraIcon';
+
 registerBlockType('my-plugin/block-two', {
     title: __('Block Two', 'my-plugin'),
     description: __('A block that displays locations from the Rick and Morty API.', 'my-plugin'),
     category: 'widgets',
-    icon: 'location-alt', // Puedes usar cualquier icono disponible en Dashicons
+    icon: AgoraIcon, // Puedes usar cualquier icono disponible en Dashicons
     supports: {
         html: false, // Deshabilitar la edición HTML
     },
