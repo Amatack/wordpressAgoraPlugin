e20a5be0a2ab5c107ca1b72f5d8f4f7af0af6d85
carlos <carlosviniciogarcia1997@hotmail.com>
2025-01-03 16:55:00 -0500

    names of functions changed

--- api/graphql/resolvers/pluginResolvers.js
+++ api/graphql/resolvers/pluginResolvers.js
@@ -2,9 +2,9 @@ import { ChronikClient } from "chronik-client";
 import { Agora } from "ecash-agora";
 import { getGenesisInfo } from '../../helpers/getGenesisInfo.js';
 import { totalTrades } from '../../helpers/totalTrades.js';
-import { lastPrice } from '../../helpers/lastPrice.js';
+import { getLastPrice } from '../../helpers/getLastPrice.js';
 import { chronikInstances } from '../../constants.js';
-import { supply } from "../../helpers/supply.js";
+import { getSupply } from "../../helpers/getSupply.js";
 import { marketCap } from "../../helpers/marketCap.js";
 
 let chronikInstancesArray = chronikInstances.split(' ');
@@ -22,34 +22,36 @@ const pluginResolvers = {
                 result.totalTrades = await totalTrades(agora, tokenId);
             }
             if (include.lastPrice) {
-                result.lastPrice = await lastPrice(agora, tokenId);
+                result.lastPrice = await getLastPrice(agora, tokenId);
             }
             let decimals = -1
             if('genesisInfo' in result) {
                 decimals = result.genesisInfo.decimals
             }
             if (include.supply){
-                result.supply = await supply(chronik, tokenId, decimals)
+                result.supply = await getSupply(chronik, tokenId, decimals)
             }
             //Calculate Market Cap of specified etoken
             if (include.marketCap){
                 let supply
                 let currentPrice
-                let minAmount
+                let minTokenOrder
                 if(include.supply){
+                    console.log("Here")
                     supply=result.supply
                 }else{
-                    supply=await supply(chronik, tokenId, decimals)
+                    console.log("Here2")
+                    supply=await getSupply(chronik, tokenId, decimals)
                 }
                 if(include.lastPrice){
-                    currentPrice = result.lastPrice.price
-                    minAmount = result.lastPrice.minAmount
+                    currentPrice = result.lastPrice.minXecOrder
+                    minTokenOrder = result.lastPrice.minTokenOrder
                 }else{
-                    let lastPrice = await lastPrice(agora, tokenId);
-                    currentPrice = lastPrice.price
-                    minAmount = lastPrice.minAmount
+                    let lastPrice = await getLastPrice(agora, tokenId);
+                    currentPrice = lastPrice.minXecOrder
+                    minTokenOrder = lastPrice.minTokenOrder
                 }
-                result.marketCap = await marketCap(supply, currentPrice, minAmount)
+                result.marketCap = await marketCap(supply, currentPrice, minTokenOrder)
             }
             return result;
         }
