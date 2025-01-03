20c905a8ce8e19e30f263948df7456232a62d4c8
carlos <carlosviniciogarcia1997@hotmail.com>
2025-01-03 12:55:52 -0500

    cleaner environment

--- plugin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '63e32a316d43b93f45a9');
--- plugin/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '2db7b389e9c5b19ddd5b');
