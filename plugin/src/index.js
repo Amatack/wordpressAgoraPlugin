c4680e05fea2dee5f1f523b0910c59a8904036ec
carlos <carlosviniciogarcia1997@hotmail.com>
2025-01-04 22:01:10 -0500

    added name of remaining blocks

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,4 +1,6 @@
 import './blocks/block-price';
-import './blocks/block-two';
+import './blocks/block-genesisInfo';
+import './blocks/block-supply';
+import './blocks/block-marketCap';
 import './style.scss';
 import './editor.scss';
